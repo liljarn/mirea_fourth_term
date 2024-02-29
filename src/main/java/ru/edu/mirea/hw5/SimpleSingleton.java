56e5c5bc811fa38d3ba7c5d00cf17dfcb6a66b49
Maksim Ustinov <ustiinovmp@gmail.com>
2024-02-29 05:46:12 +0300

    added tests

--- src/main/java/ru/edu/mirea/hw5/SimpleSingleton.java
+++ src/main/java/ru/edu/mirea/hw5/SimpleSingleton.java
@@ -1,5 +1,8 @@
 package ru.edu.mirea.hw5;
 
 public class SimpleSingleton {
-    private static final SimpleSingleton INSTANCE = new SimpleSingleton();
+    public static final SimpleSingleton INSTANCE = new SimpleSingleton();
+
+    private SimpleSingleton() {
+    }
 }
