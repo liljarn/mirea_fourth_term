edbd64a8ae58e580c0054fa55618c9b6cbb16f72
Maksim Ustinov <ustiinovmp@gmail.com>
2024-03-21 09:53:58 +0300

    task 9 done

---
+++ src/main/java/ru/edu/mirea/task8/strategy/ConcreteStrategyB.java
@@ -0,0 +1,8 @@
+package ru.edu.mirea.task8.strategy;
+
+public class ConcreteStrategyB implements Strategy {
+    @Override
+    public void execute() {
+        System.out.println("Executing strategy B");
+    }
+}
